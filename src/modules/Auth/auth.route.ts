b07af7d907a1374e6e8d5ac378efb598e7e81567
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-05-21 12:39:03 +0600

    feature/login user implemented

--- src/modules/Auth/auth.route.ts
+++ src/modules/Auth/auth.route.ts
@@ -4,6 +4,7 @@ import authController from './auth.controller';
 const route: Router = Router();
 
 route.post("/signup", authController.signUpUser)
+route.post('/login', authController.loginUser);
 
 
 export const authRoute = route;
